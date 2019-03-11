24b50c6ecf4e7fa819bed576d976102bc50552a7
Rahul Kulhalli <rahulkulhalli@gmail.com>
2019-03-11 12:13:50 +0530

    Add files via upload

---
+++ Imaging/backup/main.py
@@ -0,0 +1,128 @@
+# Imports
+import os, argparse
+
+import numpy as np
+import cv2
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from keras import models
+
+import model_utils
+import visualizers
+import image_utils
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--image_name', type=str, help='Filename of the input image', default='')
+    parser.add_argument('--save_path', type=str, help='Directory to save output images in.', default='.')
+
+    parser.add_argument('--dataset', type=str, help='Dataset to visualize e.g. Malaria, Pneumonia', default='malaria')
+
+    parser.add_argument('--model_filename', type=str, help='Model Filename. Defaults to Malaria.',
+                        default='/home/ubuntu/semic-web/image/Models/malaria.h5')
+
+    parser.add_argument('--deployment', type=str, help='Deployment server: One of "local" or "remote". '
+                                                       'Defaults to local. Local denotes any instance'
+                                                       'without GPU or < 64GB memory',
+                        default='remote')
+
+    args = parser.parse_args()
+
+    return args
+
+args = get_args()
+
+assert args.image_name, 'Please provide path to input image.'
+image_name = args.image_name.split(os.path.sep)[-1]
+print (image_name)
+args.dataset = args.dataset.lower()
+
+# Assert malaria data for local compute as local compute hangs for Pneumonia
+if args.deployment == 'local':
+    assert args.dataset == 'malaria', 'LOCAL COMPUTE WILL HANG FOR PNEUMONIA'
+
+if args.dataset == 'malaria':
+    import malaria_config as config
+elif args.dataset == 'pneumonia':
+    import pneumonia_config as config
+
+input_config = config.input_config
+class_map = config.class_map
+
+model = models.load_model(args.model_filename)
+# Keep a copy of the model since modifications to model affect the original
+# model due to shallow copy
+# TODO: Optionally load on different GPU
+model_mod = models.load_model(args.model_filename)
+
+# Create wrapper model
+# TODO: Find last conv layer automatically
+print('Creating Wrapper model.')
+application_model = model_utils.ApplicationModel(model, model_mod,
+                                     make_linear=True, preprocessing_function=config.preprocessing_function,
+                                     custom_objects=config.custom_objects)
+print('Complete.')
+# Initialize visualizers
+print('Initializing Integrated Gradients.')
+integrated_vis = visualizers.IntegratedGradientsVisualizer(application_model)
+print('Initializing GradCAM.')
+grad_cam = visualizers.GradCAMVisualizer(application_model)
+
+'''
+Entry point here
+'''
+x, img = image_utils.read_image(
+    args.image_name,
+    im_framework=input_config['im_framework'],
+    target_size=input_config['target_size'],
+    mode=input_config['color_mode']
+)
+
+#TODO: Dynamic colormap
+# For now, hardcode colormap
+cmap = plt.cm.copper
+
+print('Running Inference on {}'.format(args.image_name))
+class_scores = application_model.predict(x, output='linear')
+class_probabilities, predicted_class = model_utils.softmax(class_scores)
+
+# Generate bar graph of top 5 (or less) classes
+savename = image_name.rpartition('.')[0] + '_prob_graph.png'
+image_utils.generate_bargraph(class_probabilities, class_map, os.path.join(args.save_path, savename))
+
+# Print probabilities
+print_str = 'Prediction:\n'
+for idx, _class in class_map.items():
+    print_str += '{}: {}\n'.format(_class, class_probabilities[idx])
+print(print_str)
+
+print('Generating Explanations.')
+integrated_image = integrated_vis.calculate(x, class_scores, predicted_class,
+                                            reference_image=input_config['reference_image'],
+                                            cmap=cmap, percentile=99)
+print('1 / 2')
+actmap_image = grad_cam.calculate(x, class_scores, predicted_class)
+print('2 / 2')
+
+# Save to disk
+# Integrated Gradients
+print (args.save_path)
+savename = image_name.rpartition('.')[0] + '_exp2.png'
+mask = integrated_image[..., -1] > 0
+beta = np.ones(x.shape[1:3])
+beta[mask] = 0.5
+beta = np.expand_dims(beta, axis=-1)
+super_image = (beta * x[0, :, :, ::-1] + (1 - beta) * integrated_image[..., :-1]).astype(np.uint8)
+cv2.imwrite(os.path.join(args.save_path, savename), super_image)
+
+savename = image_name.rpartition('.')[0] + '_exp3.png'
+mask = actmap_image[..., -1] > 0
+beta = np.ones(x.shape[1:3])
+beta[mask] = 0.8
+beta = np.expand_dims(beta, axis=-1)
+super_image = (beta * x[0, :, :, ::-1] + (1 - beta) * actmap_image[..., :-1]).astype(np.uint8)
+cv2.imwrite(os.path.join(args.save_path, savename), super_image)
+
+print('Complete.')
---
+++ Imaging/backup/model_utils.py
@@ -0,0 +1,114 @@
+import os, tempfile
+
+import numpy as np
+# import cv2
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+from keras import activations, models
+
+class ApplicationModel():
+
+    def __init__(self, model, model_mod, make_linear, preprocessing_function, custom_objects=None):
+
+        self.custom_objects = custom_objects
+        self.preprocessing_function = preprocessing_function
+
+        self.model = model
+        self.make_linear = make_linear
+        if make_linear:
+            self.model_mod = self.linear_output(model_mod)
+        else:
+            self.model_mod = model_mod
+
+        model_modifier = GuidedModelModifier()
+        self.guided_model = model_modifier.modify_model_backprop(
+            self.model_mod, 'guided', custom_objects=self.custom_objects
+        )
+
+        # TODO: Find last conv layer automatically
+        self.last_conv_layer = model_mod.layers[12].name
+
+    def predict(self, x, output='logits', verbose=0):
+
+        if output == 'logits':
+            preds = self.model.predict(self.preprocessing_function(x))[0]
+        elif output == 'linear':
+            preds = self.model_mod.predict(self.preprocessing_function(x))[0]
+
+        if verbose == 1:
+            print(preds)
+
+        return preds
+
+    def linear_output(self, model_non_l):
+
+        model_non_l.layers[-1].activation = activations.linear
+        model_non_l.save('temp_inc.h5')
+        model_l = models.load_model('temp_inc.h5', custom_objects=self.custom_objects)
+        os.remove('temp_inc.h5')
+        return model_l
+
+    # def get_last_conv(self):
+    #
+
+
+class GuidedModelModifier():
+
+    def __init__(self):
+        self._MODIFIED_MODEL_CACHE = dict()
+        self._BACKPROP_MODIFIERS = {
+            'guided': self._register_guided_gradient,
+            'rectified': self._register_rectified_gradient
+        }
+
+    def _register_guided_gradient(self, name):
+        if name not in ops._gradient_registry._registry:
+            @tf.RegisterGradient(name)
+            def _guided_backprop(op, grad):
+                dtype = op.outputs[0].dtype
+                gate_g = tf.cast(grad > 0., dtype)
+                gate_y = tf.cast(op.outputs[0] > 0., dtype)
+                return gate_y * gate_g * grad
+
+    def _register_rectified_gradient(self, name):
+        if name not in ops._gradient_registry._registry:
+            @tf.RegisterGradient(name)
+            def _relu_backprop(op, grad):
+                dtype = op.outputs[0].dtype
+                gate_g = tf.cast(grad > 0., dtype)
+                return gate_g * grad
+
+    def modify_model_backprop(self, model, backprop_modifier, custom_objects=None):
+        modified_model = self._MODIFIED_MODEL_CACHE.get((model, backprop_modifier))
+        if modified_model is not None:
+            return modified_model
+
+        model_path = os.path.join(tempfile.gettempdir(), next(tempfile._get_candidate_names()) + '.h5')
+        try:
+            # 1. Save original model
+            model.save(model_path)
+
+            # 2. Register modifier and load modified model under custom context.
+            modifier_fn = self._BACKPROP_MODIFIERS.get(backprop_modifier)
+            if modifier_fn is None:
+                raise ValueError("'{}' modifier is not supported".format(backprop_modifier))
+            modifier_fn(backprop_modifier)
+
+            # 3. Create graph under custom context manager.
+            with tf.get_default_graph().gradient_override_map({'Relu': backprop_modifier}):
+                #  This should rebuild graph with modifications.
+                modified_model = models.load_model(model_path, custom_objects=custom_objects)
+
+                # Cache to improve subsequent call performance.
+                self._MODIFIED_MODEL_CACHE[(model, backprop_modifier)] = modified_model
+                return modified_model
+        finally:
+            os.remove(model_path)
+
+def softmax(y):
+    max_score_idx = np.argmax(y)
+    max_score = y[max_score_idx]
+    stable_y = y - max_score
+    exponentials = np.exp(stable_y)
+    return (exponentials / np.sum(exponentials), max_score_idx)
